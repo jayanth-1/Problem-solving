75afab6d1004eb88f0662859ea07c22d97052cdd
jayanth-1 <apoorjayanth1@gmail.com>
2018-09-10 12:25:56 +0530

    updated

--- Improve/OOPS/src/TestInheritance.java
+++ Improve/OOPS/src/TestInheritance.java
@@ -4,7 +4,7 @@ public class TestInheritance {
         //creating Dog object
         Animal dogObj = new Dog();
 
-        //dog object acquires properties of parent class i.e Animal class here accessing eat method in Animal class
+        //dog object acquires properties of parent class i.e Animal class 
         dogObj.eat();
 
     }
